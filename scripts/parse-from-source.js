7997a2da73f18622ffa1d393f223c3140a19041d
khalil Migallos <kmigallos@gmail.com>
2022-10-22 17:13:38 +0800

    complete parser;

--- scripts/parse-from-source.js
+++ scripts/parse-from-source.js
@@ -1,24 +1,52 @@
 import axios from 'axios';
 import fs from 'fs';
 
-const rawLink = `https://raw.githubusercontent.com/sudheerj/reactjs-interview-questions/359af1a719f83e7f341cfbeb039147153f1f3b38/README.md`;
+const rawUrl = `https://raw.githubusercontent.com/sudheerj/reactjs-interview-questions/359af1a719f83e7f341cfbeb039147153f1f3b38/README.md`;
+const matcher = /([1-9]|[1-9][0-9]|[1-9][0-9][0-9]|1000)+\.+ ###/;
+
+const urlList = rawUrl.split('/');
+const folder = './data/';
+const rawFolder = './data/raw/';
+const filename = urlList[urlList.length - 2];
 
 run();
 
 async function run() {
-  const list = rawLink.split('/');
-  const rawFolder = './data/raw/';
-  const rawFilename = list[list.length - 2] + '.md';
-  let rawData;
+  let rawData = '';
 
-  if (fs.existsSync(rawFolder + rawFilename)) {
-    rawData = await fs.readFileSync(rawFolder + rawFilename, 'utf8');
+  if (fs.existsSync(rawFolder + filename + '.md')) {
+    rawData = await fs.readFileSync(rawFolder + filename + '.md', 'utf8');
   } else {
-    rawData = await axios.get(rawLink).then((res) => res.data);
+    rawData = await axios.get(rawUrl).then((res) => res.data);
 
-    fs.writeFile(rawFolder + rawFilename, rawData, function (err) {
+    fs.writeFile(rawFolder + filename + '.md', rawData, function (err) {
       if (err) return console.log(err);
-      console.log('saved raw to', rawFolder + rawFilename);
+      console.log('saved raw to', rawFolder + filename + '.md');
     });
   }
+
+  const parsedData = rawData
+    .split(matcher)
+    .filter((s) => parseInt(s).toString() === 'NaN')
+    .map(parseEntry);
+  parsedData.shift();
+
+  fs.writeFile(folder + filename + '.json', JSON.stringify(parsedData, null, 4), (err) => {
+    if (err) {
+      console.log('Error saving parsed data');
+    } else {
+      console.log('Saved parsed data');
+    }
+  });
+}
+
+function parseEntry(s, index) {
+  const id = index;
+  const string = `${id}. ## ` + s.replace('**[⬆ Back to Top](#table-of-contents)**', '').trim();
+
+  return {
+    id: index,
+    question: string.substring(0, string.indexOf('\n\n')),
+    answer: string.substring(string.indexOf('\n\n')),
+  };
 }
