7b293e55f88597998329926d73e96f27c3c36883
khalil Migallos <kmigallos@gmail.com>
2022-10-22 19:45:16 +0800

    Add questions constant

---
+++ src/data/questions.ts
@@ -0,0 +1,8 @@
+import qv1 from '../../data/359af1a719f83e7f341cfbeb039147153f1f3b38.json';
+
+export const QUESTIONS = {
+  v1: {
+    id: '359af1a719f83e7f341cfbeb039147153f1f3b38',
+    value: qv1,
+  },
+};
